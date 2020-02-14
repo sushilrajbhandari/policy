3e26bdbe5d602458d9da2c36daa4b459ba2a07f8
Sushil <sushilrajbhandari@icloud.com>
2020-02-14 16:16:37 -0500

    making axios call instead local starage

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -2,27 +2,10 @@ import React,{useState, useEffect} from "react";
 import { Form, Button} from "react-bootstrap";
 import {Redirect} from "react-router-dom"
 
-import data from "../db/db.json"
-
-
 const LogIn = props => {
 
-
-
-
-
-  const {autoRegistration, users} = data
-
+  //const {autoRegistration, users} = data
   const [autoRegistrationPage, setAutoRegistrationPage] = useState(false)
-
-  // localStorage.setItem('myCat', 'Tom');
-
-  window.localStorage.setItem("autoRegistration",JSON.stringify(autoRegistration) )
-
-  window.localStorage.setItem("users",JSON.stringify(users) )
-
-
-
 
   const userData = {
     "email":"",
@@ -30,56 +13,64 @@ const LogIn = props => {
     "id":""
   }
 
-
-
-
   const [credential, setCredential] = useState(userData)
-
 
   const handleChange = e => {
       const {value, name} = e.target;
       setCredential(prev => ({...prev, [name]: value}))
   }
 
-  const onLogin = () => {
-
-    let user = users[0]
-    debugger
-    if(credential.email === user.email && credential.password === user.password){
-      setAutoRegistrationPage(true)
+  const onLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const a = await fetch ("http://localhost:3000/users")
+      const respomse = await a.json()
+      if(credential.email === respomse.email && credential.password === respomse.password){
+        setAutoRegistrationPage(true)
+      }
+      console.log(respomse)
+    } catch (e){
+      console.log("error")
     }
-
-
   }
 
-
-
-
   return (
-
     <>
+      {autoRegistrationPage?<Redirect to='/customerAndVehicleDetail' />:
+        <div className="Login">
+          <Form>
+              <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control 
+                    value = {credential.email} 
+                    name="email" 
+                    type="email" 
+                    placeholder="Enter email"  
+                    onChange={handleChange}
+                  />
+              </Form.Group>
 
-    {autoRegistrationPage?<Redirect to='/customerAndVehicleDetail' />:<div className="Login">
-      <Form>
-          <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control value = {credential.email} name="email" type="email" placeholder="Enter email"  onChange={handleChange}/>
-          </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control  
+                    value = {credential.password} 
+                    name="password" 
+                    type="password" 
+                    placeholder="Password" 
+                    onChange={handleChange}
+                  />
+              </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control  value = {credential.password} name="password" type="password" placeholder="Password" onChange={handleChange}/>
-          </Form.Group>
-
-          <Button variant="primary" type="submit" onClick={onLogin}>Submit</Button>
-      </Form>
-    </div>
-    }
-
+              <Button 
+                variant="primary" 
+                type="submit" 
+                onClick={onLogin}>
+                  Submit
+              </Button>
+          </Form>
+        </div>
+      }
     </>
-
-
-
   );
 }
 
